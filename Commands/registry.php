48d1f8cb95e6f25eb5d211b5701752d0c66ec1ce
fununnn <dx94ke.tmu@gmail.com>
2024-07-27 11:04:49 +0900

    make_backup

--- Commands/registry.php
+++ Commands/registry.php
@@ -5,4 +5,5 @@ return [
     Commands\Programs\DbWipe::class,
     Commands\Programs\BookSearch::class,
     Commands\Programs\GenerateCommands::class, 
+    Commands\Programs\DatabaseBackup::class,
 ];
